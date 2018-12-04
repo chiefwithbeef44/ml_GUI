fe52fdabc1a952d320770263b53e871ba5e27784
chiefwithbeef <21036@ndhs.org>
2018-12-04 12:51:19 -0800

    updated fileReader to remove errors

--- src/com/csp/fileReader.java
+++ src/com/csp/fileReader.java
@@ -5,14 +5,21 @@ import weka.core.converters.ArffLoader;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 
 public class fileReader
 {
     private BufferedReader reader;
     loadData dataLoader;
 
-    public void readFile(File file) throws NullPointerException {
+    public void readFile(File file)
+    {
         ArffLoader.ArffReader arffReader = null;
+        try {
+            arffReader = new ArffLoader.ArffReader(reader);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         Instances data = null;
         data = arffReader.getData();
         System.out.println(arffReader.getStructure());
