d48c2e45a0fd3d4eb31a51c84fd15c8fb6f6db3a
chiefwithbeef <21036@ndhs.org>
2018-12-09 23:02:27 -0800

    found nulls, trying to find a fix

--- src/com/csp/Main.java
+++ src/com/csp/Main.java
@@ -4,6 +4,7 @@ import com.csp.clusterer_classifier.dataClusterer;
 import com.csp.model.Model;
 import com.csp.reader_loader.fileReader;
 import com.csp.reader_loader.loadData;
+import weka.core.Instances;
 
 import java.io.File;
 import java.io.IOException;
@@ -16,78 +17,79 @@ public class Main
     private static Model model = new Model();
     private static File trainFile = dataLoader.helpTrain();
     private static File testFile = dataLoader.helpTest();
-
+    private static Instances trainData;
     public static void main(String[] args)
     {
         dataLoader.setTempPath();
-
-        //tries to create the file variable for the train file, and tests to see if it is there and can be read
-        try
-        {
-            dataLoader.train = new File(dataLoader.trainPath);
-        }catch (NullPointerException e)
-        {
-            e.printStackTrace();
-        }
-        System.out.println("train can be read: "+dataLoader.train.canRead());
-        System.out.println("train file exists: " + dataLoader.train.exists());
-        System.out.println("train file can be written to: " + dataLoader.train.canWrite());
-        System.out.println("train file path: "+dataLoader.train.getAbsolutePath());
-        //prints a separator
-        System.out.println("-------------------------------------------------------------------------------------------");
-
-        //tries to create the file variable for the test file, and tests to see if it is there and can be read
-        try
-        {
-            dataLoader.test = new File(dataLoader.testPath);
-        }catch (NullPointerException e)
-        {
-            e.printStackTrace();
-        }
-        System.out.println("test can be read: "+dataLoader.test.canRead());
-        System.out.println("test file exists: " + dataLoader.test.exists());
-        System.out.println("test file can be written to: "+ dataLoader.test.canWrite());
-        System.out.println("test file path: "+dataLoader.test.getAbsolutePath());
-
-        //sets files in loadData
+//
+//        //tries to create the file variable for the train file, and tests to see if it is there and can be read
+//        try
+//        {
+//            dataLoader.train = new File(dataLoader.trainPath);
+//        }catch (NullPointerException e)
+//        {
+//            e.printStackTrace();
+//        }
+//        System.out.println("train can be read: "+dataLoader.train.canRead());
+//        System.out.println("train file exists: " + dataLoader.train.exists());
+//        System.out.println("train file can be written to: " + dataLoader.train.canWrite());
+//        System.out.println("train file path: "+dataLoader.train.getAbsolutePath());
+//        //prints a separator
+//        System.out.println("-------------------------------------------------------------------------------------------");
+//
+//        //tries to create the file variable for the test file, and tests to see if it is there and can be read
+//        try
+//        {
+//            dataLoader.test = new File(dataLoader.testPath);
+//        }catch (NullPointerException e)
+//        {
+//            e.printStackTrace();
+//        }
+//        System.out.println("test can be read: "+dataLoader.test.canRead());
+//        System.out.println("test file exists: " + dataLoader.test.exists());
+//        System.out.println("test file can be written to: "+ dataLoader.test.canWrite());
+//        System.out.println("test file path: "+dataLoader.test.getAbsolutePath());
+//
+//        //sets files in loadData
+//        try {
+//            dataLoader.trainData(dataLoader.helpTrain());
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        //tests read of train file
+//        try {
+//            reader.readFile(dataLoader.train);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        //tests read of test file
+//        try {
+//            reader.readFile(dataLoader.test);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        //builds 10 clusters of data
+//        try {
+//            trainData = clusterer.clusterFile(dataLoader.train);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//
+//        //builds a model of the clustered data
         try {
-            dataLoader.trainData(dataLoader.helpTrain());
+            model.create(reader.readFile(dataLoader.helpTrain()));
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        //tests read of train file
-        try {
-            reader.readFile(dataLoader.train);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        //tests read of test file
-        try {
-            reader.readFile(dataLoader.test);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        //builds 10 clusters of data
-        try {
-            clusterer.clusterFile(dataLoader.train);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        //builds a model of the clustered data
-        try {
-            Model.CreateModel.create(reader.readFile(dataLoader.helpTrain()));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            Model.trainModel.train(reader.readFile(dataLoader.helpTrain()));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//
+//        try {
+//            model.train(trainData);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
     }
 }
--- src/com/csp/model/Model.java
+++ src/com/csp/model/Model.java
@@ -1,38 +1,36 @@
 package com.csp.model;
 
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import weka.classifiers.pmml.consumer.SupportVectorMachineModel;
 import weka.core.Instances;
 import weka.core.pmml.MiningSchema;
-import weka.core.pmml.jaxbbindings.SupportVectorMachine;
-import weka.core.pmml.jaxbbindings.SupportVectors;
+import weka.core.pmml.jaxbbindings.*;
 
 import java.math.BigInteger;
 
 public class Model
 {
-
-    static SupportVectorMachine svm = new SupportVectorMachine();
-    static SupportVectors supportVectors = new SupportVectors();
-    static SupportVectorMachineModel svmModel;
-    static MiningSchema schema;
-    static Element element;
-    public static class CreateModel
-    {
-        public static void create(Instances instances)
+     public Node node;
+     private SupportVectorMachine svm = new SupportVectorMachine();
+     private SupportVectors supportVectors = new SupportVectors();
+     private SupportVectorMachineModel svmModel;
+     private MiningSchema schema;
+     public Element element;
+     public PMML pmml = new PMML();
+        public  void create(Instances instances)
         {
-            try {
-                svmModel = new SupportVectorMachineModel(element, instances, schema);
-                schema = new MiningSchema(element, instances, schema.getTransformationDictionary());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            node = element;
+            element = (Element) node;
+//            try {
+//                schema = new MiningSchema(element, instances, schema.getTransformationDictionary());
+//                svmModel = new SupportVectorMachineModel(element, instances, schema);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
         }
-    }
-
-    public static class trainModel
-    {
-        public static void train(Instances instances)
+    
+        public  void train(Instances instances)
         {
             BigInteger numAttributes = BigInteger.valueOf(785);
             int i = 0;
@@ -51,5 +49,4 @@ public class Model
                 i++;
             }
         }
-    }
 }
