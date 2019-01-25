4d1a33b7262188e0c03866bcf28f81bf756bdc0f
chiefwithbeef <21036@ndhs.org>
2019-01-25 08:41:27 -0800

    hey it works

--- src/com/csp/Eval.java
+++ src/com/csp/Eval.java
@@ -3,7 +3,10 @@ package com.csp;
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
 import weka.core.Instances;
-
+/**
+ * @author Edward Raff
+ * @author chiefwithbeef44
+ */
 public class Eval
 {
 	public static void evaluate(Classifier wekaModel, Instances train, Instances test) throws Exception
@@ -21,8 +24,8 @@ public class Eval
 			start = System.currentTimeMillis();
 			eval.evaluateModel(wekaModel, test);
 			end = System.currentTimeMillis();
-			System.out.println("\tEvaluation took " + (end - start) / 1000.0 + " seconds with an error rate " + eval.errorRate());
-
+			System.out.println("\tEvaluation took " + (end - start) / 1000.0 + " seconds with an error percentage of "
+					+ eval.errorRate() * 100);
 			System.gc();
 	}
 }
